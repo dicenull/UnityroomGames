1a42c1ff84e58d174d806f6a741558dab09833a7
dicenull <dicenull.dev@gmail.com>
2025-11-19 22:08:58 +0900

    プレイヤーの横移動

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,40 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float Speed = 10f;
+
     void Update()
     {
         var key = Keyboard.current;
-        if (key[Key.Space].wasPressedThisFrame)
+        if (key[Key.LeftArrow].isPressed)
         {
-            GetIt.Instance.Get<IGameData>().GameOver();
+            Move(Vector3.left);
         }
+
+        if (key[Key.RightArrow].isPressed)
+        {
+            Move(Vector3.right);
+        }
+
+        CheckOverflow();
+    }
+
+    void CheckOverflow()
+    {
+        var pos = transform.position;
+        var threshold = 10f;
+        if (Math.Abs(pos.x) > threshold)
+        {
+            pos.x *= -1;
+        }
+        transform.position = pos;
+    }
+
+    void Move(Vector3 direction)
+    {
+        transform.position += direction * Speed * Time.deltaTime;
     }
 }
