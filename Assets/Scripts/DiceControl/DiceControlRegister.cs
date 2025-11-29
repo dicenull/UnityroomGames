1e9b38d0a4eff6653f1a0351e0e4ae69a31afd03
dicenull <dicenull.dev@gmail.com>
2025-11-29 12:37:12 +0900

    スコア記録機能

--- Assets/Scripts/DiceControl/DiceControlRegister.cs
+++ Assets/Scripts/DiceControl/DiceControlRegister.cs
@@ -1,9 +1,25 @@
+using R3;
 using UnityEngine;
+using unityroom.Api;
 
 public class DiceControlRegister : MonoBehaviour
 {
     void Awake()
     {
         GetIt.Instance.Register<IGameData, DiceControlGameData>(new DiceControlGameData());
+    }
+
+    void Start()
+    {
+        var gameData = GetIt.Instance.Get<DiceControlGameData>();
+
+        gameData.IsGameOver.Subscribe(isGameOver =>
+        {
+            if (isGameOver)
+            {
+                var score = gameData.TimeScore.Value;
+                UnityroomApiClient.Instance.SendScore(1, score, ScoreboardWriteMode.HighScoreDesc);
+            }
+        }).AddTo(this);
     }
 }
