218cf88876f6d3b4341c791865b24a558adef438
dicenull <dicenull.dev@gmail.com>
2025-12-12 15:27:11 +0900

    インベーダーを追加

---
+++ Assets/Scripts/InvGame/InvSpawner.cs
@@ -0,0 +1,25 @@
+using Mono.Cecil.Cil;
+using UnityEngine;
+
+public class InvSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject inv;
+    void Start()
+    {
+        for (var y = 0; y < 3; y++)
+        {
+            for (var x = 0; x < 10; x++)
+            {
+                var instance = Instantiate(inv, transform.position + new Vector3(x, y), Quaternion.identity);
+                instance.transform.localScale = Vector3.one * 0.3f;
+                instance.transform.parent = transform;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
